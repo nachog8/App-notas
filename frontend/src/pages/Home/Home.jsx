40db2591961508f5a8295967dbac09a0b4c15ff6
Ignacio Guridi <nacho_g88@hotmail.com>
2024-09-30 19:22:16 -0300

    vinculacion login y register

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../utils/axiosInstance";
 import Modal from "react-modal";
 
 // Asegurar accesibilidad configurando el elemento principal de la app
@@ -15,9 +17,33 @@ const Home = () => {
     data: null,
   });
 
+  const [userInfo, setUserInfo] = useState(null);
+
+  const navigate = useNavigate();
+
+  //Get User Info
+  const getUserInfo = async () => {
+    try {
+      const response = await axiosInstance.get("/get-user");
+      if (response.data && response.data.user) {
+        setUserInfo(response.data.user);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        localStorage.clear();
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+    return () => {};
+  }, []);
+
   return (
     <>
-      <NavBar />
+      <NavBar userInfo={userInfo} />
 
       <div className="container mx-auto">
         {/* Estilo de la grilla */}
--- frontend/src/pages/SingUp/SingUp.jsx
+++ frontend/src/pages/SingUp/SingUp.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PasswordInput from "../../components/Input/PasswordInput";
+import axiosInstance from "../utils/axiosInstance";
 import { validateEmail } from "../utils/helper";
 
 const SingUp = () => {
@@ -9,6 +10,8 @@ const SingUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleSingUp = async (e) => {
     e.preventDefault();
@@ -29,7 +32,37 @@ const SingUp = () => {
     }
 
     setError("");
+
     //singUp Api Call
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email: email,
+        password: password,
+      });
+
+      //Handle successful registration response
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return;
+      }
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      //Handle login error
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
+      } else {
+        setError("Algo salió mal. Por favor, inténtalo de nuevo.");
+      }
+    }
   };
 
   return (
